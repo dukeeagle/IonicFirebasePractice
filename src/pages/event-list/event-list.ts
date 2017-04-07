a53d8c58ba6e29b3ef30ae278356a82c07bb55bb
Luke Igel <luke.igel@gmail.com>
2017-04-06 20:06:24 -0500

    searchbar functional!

--- src/pages/event-list/event-list.ts
+++ src/pages/event-list/event-list.ts
@@ -15,8 +15,49 @@ import { EventData } from '../../providers/event-data';
 })
 export class EventListPage {
   public eventList: any;
+  public eventRef: any;
+  public loadedList: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public eventData: EventData) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams, public eventData: EventData) {
+    this.eventRef = this.eventData.getEventList();
+
+    this.eventRef.on('value', eventList => {
+      let events = [];
+      eventList.forEach( event => {
+        events.push(event.val());
+      });
+
+      this.eventList = events;
+      this.loadedList = events;
+    });
+  }
+
+  //searchbar stuff------------
+  initializeItems(){
+    this.eventList = this.loadedList;
+  }
+
+  getItems(searchbar){
+    //reset items back to all of the items
+    this.initializeItems();
+
+    //set q to the value of the searchbar
+    var q = searchbar.srcElement.value;
+
+    //if the value is an empty string, don't filter the items
+    if(!q){
+      return;
+    }
+
+    this.eventList = this.eventList.filter((v) => {
+      if(v.name && q){
+        if(v.name.toLowerCase().indexOf(q.toLowerCase()) > -1){
+          return true;
+        }
+        return false;
+      }
+    });
+  }
 
   ionViewDidEnter(){
     this.eventData.getEventList().on('value', snapshot => {
